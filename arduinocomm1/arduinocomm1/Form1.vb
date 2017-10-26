fd34587a2d820127ddeb9c4412ceba89467d736e
Konstantinos Chatzis <kostaschatzis01@gmail.com>
2017-10-26 23:26:20 +0300

    Add files via upload

---
+++ arduinocomm1/arduinocomm1/Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System
+Imports System.IO.Ports
+
+Public Class Form1
+    Dim comPORT As String
+    Dim receivedData As String = ""
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = False
+        comPORT = ""
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        comPORT = TextBox1.Text.ToString()
+
+        If (Button1.Text = "Connect") Then
+            If (comPORT <> "") Then
+                SerialPort1.Close()
+                SerialPort1.PortName = comPORT
+                SerialPort1.BaudRate = 9600
+                SerialPort1.DataBits = 8
+                SerialPort1.Parity = Parity.None
+                SerialPort1.StopBits = StopBits.One
+                SerialPort1.Handshake = Handshake.None
+                SerialPort1.Encoding = System.Text.Encoding.Default
+                SerialPort1.ReadTimeout = 10000
+
+                SerialPort1.Open()
+                Button1.Text = "Dis-connect"
+                Timer1.Enabled = True
+                Label2.Text = "Timer: ON"
+            Else
+                MsgBox("Select a COM port first")
+            End If
+        Else
+            SerialPort1.Close()
+            Button1.Text = "Connect"
+            Timer1.Enabled = False
+            Label2.Text = "Timer: OFF"
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        receivedData = ReceiveSerialData()
+        RichTextBox1.Text = receivedData
+    End Sub
+
+    Function ReceiveSerialData() As String
+        Dim Incoming As String
+        Try
+            Incoming = SerialPort1.ReadExisting()
+            If Incoming Is Nothing Then
+                Return "nothing" & vbCrLf
+            Else
+                Return Incoming
+            End If
+        Catch ex As TimeoutException
+            Return "Error: Serial Port read timed out."
+        End Try
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        RichTextBox1.Text = ""
+    End Sub
+End Class
